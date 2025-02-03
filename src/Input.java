4f00b3866219cd9ffa152994d54b2d0ee84a4269
Ismael Velazquez <128411935+Ivela183@users.noreply.github.com>
2025-02-03 02:16:01 +0000

    First Commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");//Prompting user to input their name
+        String name;
+        name = scanner.nextLine();
+        System.out.print("Enter your age: ");//Prompting user to input their age
+        int age;
+        age = scanner.nextInt();
+        System.out.print("Enter your weight: ");//Prompting user to input their weight
+        double weight;
+        weight = scanner.nextDouble();
+        System.out.print("Do you smoke? True/False: ");//Prompting user to input if they smoke or not
+        boolean smoker;
+        smoker = scanner.nextBoolean();
+        System.out.print("\nYour name is " + name + ", " + age + " years old, " + "who weights " + weight + ", and smoker:" + smoker);//Displaying the user's information
 
+
+    }
+}
